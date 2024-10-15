6eef21027d35f958857e80d3c028d13cad68a39c
ElenaMoskvina <113961800+ElenaMoskvina@users.noreply.github.com>
2024-10-15 15:39:18 +0500

    Update Lab2.java

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -11,11 +11,11 @@ public class Lab2 {
 public static void main (String[] args) throws IOException  {
 	
 	//Прочитать из файла параметры функции: q - количество ошибок, m - интервал времени до ошибки 
-	System.out.println ("Данные файла lab2.txt");
-	System.out.println ("q  |"+ "m  |");
+	//System.out.println ("Данные файла lab2.txt");
+	//System.out.println ("q  |"+ "m  |");
 	
 	//// Получение данных из файла
-	Path filePath = Paths.get("lab2.txt");
+	Path filePath = Paths.get("lab2/lab2.txt");
 	Scanner scanner = new Scanner(filePath);
 	
 	////Запись в список и вывод в консоль
@@ -32,7 +32,7 @@ public static void main (String[] args) throws IOException  {
 			String formattedBugNumber = new DecimalFormat("00").format(bugNumber.get(i));
 			String formattedBugTime = new DecimalFormat("00").format(bugTime.get(i));
 			
-			System.out.println (formattedBugNumber+" |"+formattedBugTime+" |");
+			//System.out.println (formattedBugNumber+" |"+formattedBugTime+" |");
 			
 		}
 		else {
@@ -42,32 +42,31 @@ public static void main (String[] args) throws IOException  {
 	
 
 	//Считать из консоли интервал [a, b]и погрешность e
-	
-	
-	float a = 26;
-		
-	float b = 100;
-	
-	float e = (float)0.001;
+	//Scanner in = new Scanner(System.in);
+	//System.out.println ("Введите нижнюю границу интервала");
+	double a = Double.valueOf(args[0]);
+	//System.out.println ("Введите верхнюю границу интервала");
+	double b = Double.valueOf(args[1]);
+	//System.out.println ("Введите желаемую погрешность");
+	double e = Double.valueOf(args[2]);
 	
 	//Вычислить общее количество ошибок в программе
 	NumberOfBugs numberOfBugs = new NumberOfBugs();
-	float c = numberOfBugs.BugsCalc(a, b, e, bugTime, bugNumber);
+	double c = numberOfBugs.BugsCalc(a, b, e, bugTime, bugNumber);
 	
 	
 	//Вычислить коэффициент пропорциональности	
 	ScaleFactor scaleFactor = new ScaleFactor();
-	float k1= scaleFactor.ScaleFactorCalc(c, bugTime, bugNumber );
+	double k1= scaleFactor.ScaleFactorCalc(c, bugTime, bugNumber );
 		
 		
 	//Вычислить среднее время до появления ошибки	
 	AverageBugTime averageBugTime = new AverageBugTime();
-	float t = averageBugTime.AverageBugTimeCalc(c, k1, bugTime);
+	double t = averageBugTime.AverageBugTimeCalc(c, k1, bugTime);
 	
 	
 	//Вычислить время до начала тестирования
 	TestingStartTime testingStartTime  = new TestingStartTime();
-	float tt =  testingStartTime.TestingStartTimeCalc (c, k1, bugTime, bugNumber);
+	double tt =  testingStartTime.TestingStartTimeCalc (c, k1, bugTime, bugNumber);
 	}
 }
-
